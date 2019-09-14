e9f3622571c7715d5edc4433f6f5d93e1f59f706
dgirty <dggirty@gmail.com>
2019-09-14 15:59:33 +0000

    flixter39-1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  https://flixter-darrell-girty.herokuapp.com/
+  config.action_mailer.default_url_options = { host: 'flixter-darrell-girty.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
